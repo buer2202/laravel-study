925814348ce3dc1d4364e883a7fba270681ad882
buer2202 <buer2202@163.com>
2016-09-17 10:03:00 +0800

    laravel 5.3

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::group(['middleware' => 'auth.admin'], function () {
+        Route::get('/', 'Admin\IndexController@index');
+    });
+
+    Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'Admin\LoginController@login');
+    Route::post('logout', 'Admin\LoginController@logout');
+});
+
+Route::get('about-us', function () {
+    return 'page-about-us';
+});
+
+Route::get('dashboard', function () {
+    return 'page-dashboard';
+});
+
+Route::get('register', function () {
+    return 'page-register';
+});
